d708244b4288317bb92880cfa2253609e92e7638
sandrina-p <sandrin4pereir4@gmail.com>
2020-03-31 15:04:24 +0100

    make linters work

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
+// import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
 import { PapersContextProvider, loadProfile } from './store/PapersContext.js';
