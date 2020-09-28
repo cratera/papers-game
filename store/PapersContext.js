c785ab4c48d05e3bec76eec9642817c76e0caf8a
sandrina-p <sandrin4pereir4@gmail.com>
2020-09-28 22:49:45 +0100

    0.2.5@13 - Add profile.stats

--- store/PapersContext.js
+++ store/PapersContext.js
@@ -68,7 +68,7 @@ export class PapersContextProvider extends Component {
       profiles: {}, // List of game players' profiles.
       about: {
         version: '0.2.5',
-        ota: '12',
+        ota: '13',
       },
     }
 
