cb5aed47ed3051758f3a5be06f9ce9c6ee4301cc
João Rodrigues <joaocdvrodrigues@gmail.com>
2023-05-04 20:25:44 +0100

    feat: add aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ['.'],
+          alias: {
+            // NOTE: we need to use the same alias as in tsconfig.json
+            '@constants': './src/constants',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   }
 }
