8126291b3574de187c7b9b13e77c8ac994bd474e
João Rodrigues <joaocdvrodrigues@gmail.com>
2023-06-05 22:01:11 +0000

    chore: remove duplicated firebase config
    I believe this is uneccessary, the game runs just fine without it

--- app.config.ts
+++ app.config.ts
@@ -29,18 +29,6 @@ const config: ExpoConfig = {
   },
   web: {
     favicon: './assets/images/icon.png',
-    config: {
-      firebase: {
-        apiKey: 'AIzaSyBYB6fczaHoB-SKjMRM4VHradIyLtluBHM',
-        authDomain: 'papers-game.firebaseapp.com',
-        databaseURL: 'https://papers-game.firebaseio.com',
-        projectId: 'papers-game',
-        storageBucket: 'papers-game.appspot.com',
-        messagingSenderId: '381121722531',
-        appId: '1:381121722531:web:5d88b1a0b418e4e8c8e716',
-        measurementId: 'G-BFBZ4MNDX4',
-      },
-    },
   },
   extra: {
     eas: {
