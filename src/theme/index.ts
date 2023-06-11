83d1b2cbf1821dd59baa005d84e5e7e85bbc43be
João Rodrigues <joaocdvrodrigues@gmail.com>
2023-06-11 10:13:46 +0000

    feat: create temporary spacing classes
    So that I can get rid of inline-style errors easily, these will be replaced by tailwind or another ui library later on

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,8 @@
 import * as animations from './animations'
 import colors from './colors'
 import fontSize from './fontSize'
+import spacing from './spacing'
 import typography from './typography'
 import utils from './utils'
 
-export { animations, colors, fontSize, typography, utils }
+export { animations, colors, fontSize, spacing, typography, utils }
---
+++ src/theme/spacing.ts
@@ -0,0 +1,139 @@
+import { StyleSheet } from 'react-native'
+
+// Temporary solution so that we don't have to create classes for each spacing, replaced by TailwindCSS later on
+export default StyleSheet.create({
+  // Margin
+  mt_4: {
+    marginTop: 4,
+  },
+  mt_8: {
+    marginTop: 8,
+  },
+  mt_12: {
+    marginTop: 12,
+  },
+  mt_16: {
+    marginTop: 16,
+  },
+  mt_24: {
+    marginTop: 24,
+  },
+  mt_32: {
+    marginTop: 32,
+  },
+
+  mb_4: {
+    marginBottom: 4,
+  },
+  mb_8: {
+    marginBottom: 8,
+  },
+  mb_12: {
+    marginBottom: 12,
+  },
+  mb_16: {
+    marginBottom: 16,
+  },
+  mb_24: {
+    marginBottom: 24,
+  },
+  mb_32: {
+    marginBottom: 32,
+  },
+
+  ml_4: {
+    marginLeft: 4,
+  },
+  ml_8: {
+    marginLeft: 8,
+  },
+  ml_12: {
+    marginLeft: 12,
+  },
+  ml_16: {
+    marginLeft: 16,
+  },
+  ml_24: {
+    marginLeft: 24,
+  },
+  ml_32: {
+    marginLeft: 32,
+  },
+
+  mr_4: {
+    marginRight: 4,
+  },
+  mr_8: {
+    marginRight: 8,
+  },
+  mr_12: {
+    marginRight: 12,
+  },
+  mr_16: {
+    marginRight: 16,
+  },
+  mr_24: {
+    marginRight: 24,
+  },
+  mr_32: {
+    marginRight: 32,
+  },
+
+  // Padding
+  p_4: {
+    padding: 4,
+  },
+  p_8: {
+    padding: 8,
+  },
+  p_12: {
+    padding: 12,
+  },
+  p_16: {
+    padding: 16,
+  },
+  p_24: {
+    padding: 24,
+  },
+  p_32: {
+    padding: 32,
+  },
+
+  ph_4: {
+    paddingHorizontal: 4,
+  },
+  ph_8: {
+    paddingHorizontal: 8,
+  },
+  ph_12: {
+    paddingHorizontal: 12,
+  },
+  ph_16: {
+    paddingHorizontal: 16,
+  },
+  ph_24: {
+    paddingHorizontal: 24,
+  },
+  ph_32: {
+    paddingHorizontal: 32,
+  },
+
+  pv_4: {
+    paddingVertical: 4,
+  },
+  pv_8: {
+    paddingVertical: 8,
+  },
+  pv_12: {
+    paddingVertical: 12,
+  },
+  pv_16: {
+    paddingVertical: 16,
+  },
+  pv_24: {
+    paddingVertical: 24,
+  },
+  pv_32: {
+    paddingVertical: 32,
+  },
+})
